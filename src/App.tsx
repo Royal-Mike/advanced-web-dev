5e687ba76af388677c4d6208557056103e26b4ae
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-10-20 02:35:33 +0700

    IA01: feature 3

--- src/App.tsx
+++ src/App.tsx
@@ -64,6 +64,8 @@ export default function Game() {
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
+  const [sortAscending, setSortAscending] = useState(true);
+
   function handlePlay(nextSquares: string[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
@@ -92,12 +94,17 @@ export default function Game() {
     );
   });
 
+  if (!sortAscending) {
+    moves.reverse();
+  }
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
+        <button onClick={() => setSortAscending(!sortAscending)}>Sort {sortAscending ? 'Descending' : 'Ascending'}</button>
         <ol>{moves}</ol>
       </div>
     </div>
