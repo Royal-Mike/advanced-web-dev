78b657b26c5c7af5b021673ddffc157acb35bb9d
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-10-27 13:14:12 +0700

    navigation in home page

--- src/pages/Home/app.tsx
+++ src/pages/Home/app.tsx
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate(); 
+  const routeChange = (path: string) =>{ 
+    navigate(path);
+  }
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1 className="px-4 py-2 text-2xl font-semibold">21127561</h1>
+      <button className="mx-2 px-2 border-2 border-black rounded" onClick={() => routeChange('/tic-tac-toe')}>
+        IA01: Tic-Tac-Toe
+      </button>
+      <button className="mx-2 px-2 border-2 border-black rounded" onClick={() => routeChange('/unsplash-gallery')}>
+        IA02: Unsplash Gallery
+      </button>
+    </>
   );
 }
