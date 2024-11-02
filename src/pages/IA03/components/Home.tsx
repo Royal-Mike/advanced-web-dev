841b716021c88da14900cb584e9dbd4b3cf78be5
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-11-03 02:10:17 +0700

    IA03: check access token to home

--- src/pages/IA03/components/Home.tsx
+++ src/pages/IA03/components/Home.tsx
@@ -1,4 +1,31 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const API = import.meta.env.VITE_REACT_APP_API_LOCAL;
+
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch(`${API}/user/home`, {
+          method: 'GET',
+          credentials: 'include', // Sends the HTTP-only cookie with the request
+        });
+
+        if (!response.ok) {
+          navigate('./login'); // Redirect to login if not authenticated
+        }
+      } catch (error) {
+        console.error('Authentication check failed', error);
+        navigate('./login');
+      }
+    };
+
+    checkAuth();
+  }, [navigate]);
+
   return (
 		<h1 className='text-2xl text-white'>Home</h1>
   );
