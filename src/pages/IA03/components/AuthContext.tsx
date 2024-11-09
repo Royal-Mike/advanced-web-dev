df7331562a8248c3b2973a5d61d4775b76b83fe2
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-11-10 02:22:39 +0700

    IA04: add comment

--- src/pages/IA03/components/AuthContext.tsx
+++ src/pages/IA03/components/AuthContext.tsx
@@ -42,6 +42,7 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
     if (location.pathname !== '/user-registration/register') checkAuth();
   }, [navigate]);
 
+	// Logout function to clear authentication state
   const logout = async () => {
     try {
       const response = await fetch(`${API}/user/logout`, {
