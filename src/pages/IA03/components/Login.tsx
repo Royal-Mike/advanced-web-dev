42cf2919f6b11e2c1b5a15d75d694f0c29852d6e
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-11-10 01:44:01 +0700

    IA04: fix route path

--- src/pages/IA03/components/Login.tsx
+++ src/pages/IA03/components/Login.tsx
@@ -81,7 +81,7 @@ function Login() {
 						? <ClipLoader size={30} color={"white"} loading={true} />
 						: <>
 								<a className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:ring hover:ring-white h-10 px-4 py-2 duration-200"
-									href="/user-registration/register">Register</a>
+									href="./register">Register</a>
 								<button
 									className="font-semibold hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 inline-flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black h-10 px-4 py-2"
 									type="submit">Log in</button>
